58cbd8f8859a287f43dc09b4d4c8a6fe2a77f462
KenthaiColle <ken.k2009@hotmail.com>
2022-05-16 23:19:07 +1000

    Pause menu done

--- Timely Manor/Assets/Scripts/Menu/PauseMenu.cs
+++ Timely Manor/Assets/Scripts/Menu/PauseMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using StarterAssets;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,11 +11,32 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     private DontDestroyScript[] dontDestroyObjects;
 
+    //public InputAction exit;
+    public StarterAssets.StarterAssetsInputs _input;
+    public StarterAssets.FirstPersonController _state;
+
     public GameObject PauseMenuUI;
+    public GameObject OptionMenuUI;
     // Update is called once per frame
     void Update()
     {
-        
+        if (_input.exit && _state._playerState != FirstPersonController.PlayerState.Interacting)
+        {
+            if(GameIsPaused)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                _input.exit = false;
+                Resume();
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                _input.exit = false;
+                Pause();
+            }
+        }
     }
 
 
@@ -23,6 +45,7 @@ public class PauseMenu : MonoBehaviour
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Debug.Log("Resume");
     }
 
 
@@ -36,6 +59,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
+        //Destory all don't destory object so it doesn't screw up the menu
         dontDestroyObjects = FindObjectsOfType<DontDestroyScript>();
         foreach (DontDestroyScript d in dontDestroyObjects)
         {
@@ -44,4 +68,15 @@ public class PauseMenu : MonoBehaviour
 
         SceneManager.LoadScene("Menu_Test");
     }
+    //public void LoadOption()
+    //{
+    //    OptionMenuUI.SetActive(true);
+    //    PauseMenuUI.SetActive(false);
+    //}
+
+    //public void LoadPauseMenu()
+    //{
+    //    OptionMenuUI.SetActive(false);
+    //    PauseMenuUI.SetActive(true);
+    //}
 }
