be2277dfa6cf70be5c4532482bbaaa598b7ac7d3
KenthaiColle <ken.k2009@hotmail.com>
2022-06-28 02:38:50 +1000

    New Inventory works

---
+++ Timely Manor/Assets/Scripts/Interactable/Inventory/InventorySlot.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    Item item;
+    public Image icon;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+
+        Debug.Log("The added item is: " + item);
+        Debug.Log("The added icon is: " + item.icon);
+
+        icon.enabled = true;
+        icon.sprite = item.icon;
+        //Debug.Log("The icon is added");
+        
+    } 
+
+    public void ClearSlot()
+    {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+
+}
