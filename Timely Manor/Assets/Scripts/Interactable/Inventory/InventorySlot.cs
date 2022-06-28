b71376e41ce347dff2f4a77c57b0ceba2ef3258d
KenthaiColle <ken.k2009@hotmail.com>
2022-06-28 10:56:41 +1000

    Inventory Finish
    - Added description and icon to the journal.

--- Timely Manor/Assets/Scripts/Interactable/Inventory/InventorySlot.cs
+++ Timely Manor/Assets/Scripts/Interactable/Inventory/InventorySlot.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InventorySlot : MonoBehaviour
 {
     Item item;
     public Image icon;
+
+    public Image detailsIcon;
+    public TextMeshProUGUI detailsText;
+
 
     public void AddItem(Item newItem)
     {
@@ -30,6 +35,11 @@ public class InventorySlot : MonoBehaviour
     public void DisplayDescription()
     {
         // The item des go into the next page
+        if(item != null)
+        {
+            detailsIcon.sprite = item.icon;
+            detailsText.text = item.description;
+        }
     }
 
 }
