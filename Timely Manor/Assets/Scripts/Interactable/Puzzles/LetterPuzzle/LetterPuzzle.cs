551433abbbfb0a920ac5229979d4424e79128093
KenthaiColle <ken.k2009@hotmail.com>
2022-07-25 20:53:20 +1000

    Letter Puzzle imported
    - Need to combine them together

---
+++ Timely Manor/Assets/Scripts/Interactable/Puzzles/LetterPuzzle/LetterPuzzle.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterPuzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public int currentLetter = 0;
+
+    public GameObject LetterObj;
+    public string _letter1;
+    public string _letter2;
+    public string _letter3;
+    public string _letter4;
+    public string _letter5;
+    public string _letter6;
+
+    public Renderer self;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLetter == 0)
+        {
+            //Make this letter 1
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter1;
+            //Debug.Log(currentLetter);
+        }
+        if (currentLetter == 1)
+        {
+            //Make this letter 2
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter2;
+            //Debug.Log(currentLetter);
+        }
+        if (currentLetter == 2)
+        {
+            //Make this letter 3
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter3;
+            //Debug.Log(currentLetter);
+        }
+        if (currentLetter == 3)
+        {
+            //Make this letter 4
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter4;
+            // Debug.Log(currentLetter);
+        }
+        if (currentLetter == 4)
+        {
+            //Make this letter 5
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter5;
+            // Debug.Log(currentLetter);
+        }
+        if (currentLetter == 5)
+        {
+            //Make this letter 6
+            LetterObj.GetComponent<TMPro.TextMeshPro>().text = _letter6;
+            // Debug.Log(currentLetter);
+        }
+    }
+
+    //call on the + - button 
+    public void updateCurrentLetterUp()
+    {
+        //if we do up to F, put a limit of 6 and reset it back to 0
+        
+        if(currentLetter < 6)
+        {
+            currentLetter++;
+        }
+        else
+        {
+            currentLetter = 0;
+        }
+    }
+    public void updateCurrentLetterDown()
+    {
+        if (currentLetter > 0)
+        {
+            currentLetter--;
+        }
+        else
+        {
+            currentLetter = 5;
+        }
+        
+    }
+}
---
+++ Timely Manor/Assets/Scripts/Interactable/Puzzles/LetterPuzzle/LetterPuzzleController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterPuzzleController : MonoBehaviour
+{
+    //Reference to all the letters
+    [SerializeField]
+    private LetterPuzzle _letter1;
+    [SerializeField]
+    private LetterPuzzle _letter2;
+    [SerializeField]
+    private LetterPuzzle _letter3;
+    [SerializeField]
+    private LetterPuzzle _letter4;
+    [SerializeField]
+    private LetterPuzzle _letter5;
+
+    //ref to light
+    [SerializeField]
+    private CorrectLightBulb _lightBulb;
+
+    public Transform lightBulb;
+
+    public AudioSource fail;
+    public AudioSource success;
+    // Update is called once per frame
+    public void CheckAnswer()
+    {
+        // 
+        if (_letter1.currentLetter == 2 && _letter2.currentLetter == 2 && _letter3.currentLetter == 1 && _letter4.currentLetter == 5 && _letter5.currentLetter == 1)
+        {
+            var cubeRenderer = lightBulb.GetComponent<Renderer>();
+            cubeRenderer.material.SetColor("_Color", Color.green);
+
+            _lightBulb.turnOn();
+            success.Play();
+        }
+        else
+        {
+            fail.Play();
+        }
+    }
+}
