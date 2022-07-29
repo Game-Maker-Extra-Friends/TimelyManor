9cb398399ac69a4c0704f2be20f314b95841095d
William Burns <101923920@student.swin.edu.au>
2022-07-29 16:13:05 +1000

    Bug: Now properly destroys game objects when demo is completed

--- Timely Manor/Assets/Scripts/tempScripts/continueScript.cs
+++ Timely Manor/Assets/Scripts/tempScripts/continueScript.cs
@@ -8,12 +8,19 @@ public class continueScript : MonoBehaviour
 {
     // Script for passing the intro/outro splash screens
     public string scenename;
+    private DontDestroyScript[] dontDestroyObjects;
+
     void Update()
     {
         var keyboard = Keyboard.current;
         var mouse = Mouse.current;
         if (keyboard.anyKey.wasPressedThisFrame || mouse.leftButton.wasPressedThisFrame)
 		{
+            dontDestroyObjects = FindObjectsOfType<DontDestroyScript>();
+            foreach (DontDestroyScript d in dontDestroyObjects)
+            {
+                Destroy(d.gameObject);
+            }
             SceneManager.LoadScene(scenename);
 		}
 
