567b7c5aa5a2a11f11da7d089c36a263bf39c559
PorridgeB <49393032+PorridgeB@users.noreply.github.com>
2022-08-31 13:13:37 +1000

    created mystery UI

--- Timely Manor/Assets/Scripts/UI/MysteryUI.cs
+++ Timely Manor/Assets/Scripts/UI/MysteryUI.cs
@@ -5,9 +5,43 @@ using UnityEngine;
 public class MysteryUI : MonoBehaviour
 {
     public GameObject mysteryPrefab;
+    public GameObject mysteryEntryPrefab;
+    public GameObject pageRulePrefab;
+    public Transform scrollContainer;
+    public Transform mysteryEntryContainer;
 
     private void OnEnable()
     {
-        
+        foreach (Transform t in scrollContainer)
+        {
+            Destroy(t.gameObject);
+        }
+        foreach (Mystery m in Resources.LoadAll("Mysteries")) {
+            MysteryItem instance = Instantiate(mysteryPrefab, scrollContainer).GetComponent<MysteryItem>();
+            instance.Set(m, this);
+        }
+    }
+
+    public void UpdateUI(Mystery m)
+    {
+        Debug.Log("setting inspector");
+        foreach (Transform t in mysteryEntryContainer)
+        {
+            Destroy(t.gameObject);
+        }
+        foreach (MysteryEntry e in m.entries)
+        {
+            if (e.revealed)
+            {
+                var script = Instantiate(mysteryEntryPrefab, mysteryEntryContainer).GetComponent<MysteryEntryUI>();
+                script.Set(e);
+                Instantiate(pageRulePrefab, mysteryEntryContainer);
+            }
+        }
+        if (mysteryEntryContainer.childCount == 0)
+        {
+            return;
+        }
+        Destroy(mysteryEntryContainer.GetChild(mysteryEntryContainer.childCount - 1).gameObject);
     }
 }
