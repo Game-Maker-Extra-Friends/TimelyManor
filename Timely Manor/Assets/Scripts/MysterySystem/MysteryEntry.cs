e3546c90961e94ed520b48d877d07e41dbb72e82
KenthaiColle <ken.k2009@hotmail.com>
2022-09-16 17:33:40 +1000

    Revert "Revert "Merge branch 'Brian/main'""
    This reverts commit 7872f1e3a84b4a4bab143c801c3dd3c180ae7241.

--- Timely Manor/Assets/Scripts/MysterySystem/MysteryEntry.cs
+++ Timely Manor/Assets/Scripts/MysterySystem/MysteryEntry.cs
@@ -5,7 +5,37 @@ using UnityEngine;
 [System.Serializable]
 public class MysteryEntry
 {
+    public bool eventBased;
+    public bool allRequired;
     public string text;
-    public List<Clue> triggers;
+    public List<Clue> clueTriggers;
+    public List<GameEvent> eventTriggers;
+    private int triggersMet;
     public bool revealed;
+
+    public bool Complete
+    {
+        get
+        {
+            if (eventBased || !allRequired) return triggersMet > 0;
+            return clueTriggers.Count == triggersMet;
+        }
+    }
+
+    public void CheckTriggers(GameEvent evt)
+    {
+        if (eventBased && eventTriggers.Contains(evt))
+        {
+            triggersMet++;
+        }
+    }
+
+    public void CheckTriggers(Clue clue)
+    {
+
+        if (!eventBased && clueTriggers.Contains(clue))
+        {
+            triggersMet++;
+        }
+    }
 }
