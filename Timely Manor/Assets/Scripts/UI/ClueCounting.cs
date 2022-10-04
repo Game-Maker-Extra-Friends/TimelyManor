8c481cc2873b80f820d4247708eb64c2b16ca2a1
KenthaiColle <ken.k2009@hotmail.com>
2022-10-04 23:42:58 +1100

    Clue left UI now become transparent
    - Transparent and grey when there's no clue left
    - + Added E text under it

--- Timely Manor/Assets/Scripts/UI/ClueCounting.cs
+++ Timely Manor/Assets/Scripts/UI/ClueCounting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using StarterAssets;
+using TMPro;
 
 public class ClueCounting : MonoBehaviour
 {
@@ -14,7 +15,10 @@ public class ClueCounting : MonoBehaviour
 
     public GameObject parent;
 
+    // For enableing and disabling
     public Image image;
+    public TextMeshProUGUI text;
+
     public static ClueCounting instance;
     private Collider currentCollider;
 
@@ -42,39 +46,65 @@ public class ClueCounting : MonoBehaviour
         currentCollider = col;
     }
 
+    // When walking over new Interact point
     public void updateButtonPrompt(Collider col)
     {
         updateCurrentClue(col);
         if (count == 0 && FirstPersonController.instance.playerState == FirstPersonController.PlayerState.Moving)
         {
             image.gameObject.SetActive(true);
+            text.gameObject.SetActive(true);
             image.sprite = noCluesLeft;
+
+            // Set image transparency
+            var tempColour = image.color;
+            tempColour.a = 0.3f;
+            image.color = tempColour;
         }
         else if (count > 0 && FirstPersonController.instance.playerState == FirstPersonController.PlayerState.Moving)
         {
             updateCurrentClue(currentCollider);
             image.gameObject.SetActive(true);
+            text.gameObject.SetActive(true);
             image.sprite = cluesLeft;
+
+            // Set image transparecny
+            var tempColour = image.color;
+            tempColour.a = 1f;
+            image.color = tempColour;
         }
     }
 
     public void disable()
     {
         image.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
     }
 
     void Update()
     {
         if(count == 0 && FirstPersonController.instance.playerState == FirstPersonController.PlayerState.Interacting)
         {
+            // Set image transparecny
+            var tempColour = image.color;
+            tempColour.a = 0.3f;
+            image.color = tempColour;
+
             image.gameObject.SetActive(true);
             image.sprite = noCluesLeft;
+            
         }
         else if(count > 0 && FirstPersonController.instance.playerState == FirstPersonController.PlayerState.Interacting)
         {
+            // Set image transparecny
+            var tempColour = image.color;
+            tempColour.a = 1f;
+            image.color = tempColour;
+
             updateCurrentClue(currentCollider);
             image.gameObject.SetActive(true);
             image.sprite = cluesLeft;
+
         }
     }
 }
