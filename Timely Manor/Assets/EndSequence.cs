2be49a226a2b9e30fdba2cd22bb8b337c6448b0c
William Burns <101923920@student.swin.edu.au>
2022-11-02 18:24:38 +1100

    end sequence door (bookshelf moved for ease of testing)

--- Timely Manor/Assets/EndSequence.cs
+++ Timely Manor/Assets/EndSequence.cs
@@ -10,6 +10,8 @@ public class EndSequence : MonoBehaviour
     public CinemachineDollyCart cart;
 	public CinemachineVirtualCamera vcam;
 
+    public Door door;
+
     public void Sequence()
     {
 
@@ -17,9 +19,17 @@ public class EndSequence : MonoBehaviour
         vcam.Priority = 10;
         cart.m_Position = 0;
         vcam.transform.position = cart.gameObject.transform.position;
+        StartCoroutine(OpenDoor());
         StartCoroutine(EndOfAllThings());
     }
 
+    public float doorTimer = 3f;
+
+    IEnumerator OpenDoor()
+    {
+        yield return new WaitForSeconds(doorTimer);
+        door.Open(this.transform.position);
+    }
 
     public float transitionTime = 1f;
 
